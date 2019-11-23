d05c239be64d86fe95507b6d521e1c5d28883cd6
Paul A. Cordova <royalcordova@protonmail.ch>
2019-11-23 14:11:21 -0500

    new st.nix

--- st.nix
+++ st.nix
@@ -3,8 +3,10 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
     name = "st";
     version = "0.9";
-    src = fetchurl {
-        url = "https://github.com/pacordova/st/archive/master/st-master.tar.gz";
+    src = fetchFromGitHub {
+	owner = "pacordova";
+	repo = "st";
+	rev = "a382e776c3e2ab613ad8d83d63d99b75334f9e10";
         sha256 = null;
     };
     nativeBuildInputs = [ pkgconfig ];
